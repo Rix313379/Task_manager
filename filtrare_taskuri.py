73773c05b7ce067ef82264efe97054054aae6600
Iulian Vasilescu <icvasilescu@gmail.com>
2024-10-16 20:39:35 +0300

    commit de umplutura

--- filtrare_taskuri.py
+++ filtrare_taskuri.py
@@ -1,8 +1,6 @@
 # filtrare_task-uri
-task1 = {'id': 1, 'name': 'task1', 'max_date': '10/16/2024', 'assignee': 'Iulian', 'category': 'categ1'}
-tasks_list = [task1, task2, task3]
-tasks_list = [
-    {'id': 1, 'name': 'task1', 'max_date': '10/16/2024', 'assignee': 'Iulian', 'category': 'categ1'},
-    {'id': 2, 'name': 'task1', 'max_date': '10/16/2024', 'assignee': 'Iulian', 'category': 'categ1'},
-    {'id': 3, 'name': 'task1', 'max_date': '10/16/2024', 'assignee': 'Iulian', 'category': 'categ1'}
-]
+import pandas as pd
+
+df = pd.read_csv('taskuri.csv')
+git # df_editata.to_csv('taskuri.csv', index=False)
+print(df)
