a6d5e4a173bc92eb5698db7320a97d0675300cc4
Iulian Vasilescu <icvasilescu@gmail.com>
2024-10-16 20:15:01 +0300

    Pregatire

---
+++ filtrare_taskuri.py
@@ -0,0 +1,8 @@
+# filtrare_task-uri
+task1 = {'id': 1, 'name': 'task1', 'max_date': '10/16/2024', 'assignee': 'Iulian', 'category': 'categ1'}
+tasks_list = [task1, task2, task3]
+tasks_list = [
+    {'id': 1, 'name': 'task1', 'max_date': '10/16/2024', 'assignee': 'Iulian', 'category': 'categ1'},
+    {'id': 2, 'name': 'task1', 'max_date': '10/16/2024', 'assignee': 'Iulian', 'category': 'categ1'},
+    {'id': 3, 'name': 'task1', 'max_date': '10/16/2024', 'assignee': 'Iulian', 'category': 'categ1'}
+]
