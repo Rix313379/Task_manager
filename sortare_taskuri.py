e670f7c36d04859b914b7f683164a861ce42c0c9
Violeta <alexavioleta87@gmail.com>
2024-10-23 15:16:55 +0300

    S-a adaugat fisierul sortare_taskuri

---
+++ sortare_taskuri.py
@@ -0,0 +1,38 @@
+"""
+Descriere: Utilizatorul poate sorta taskurile după următoarele criterii:
+▪ 1: Sortare ascendentă task
+▪ 2: Sortare descendentă task
+▪ 3:Sortare ascendentă dată
+▪ 4:Sortare descendentă dată
+▪ 5:Sortare ascendentă persoană responsabilă
+▪ 6: Sortare descendentă persoană responsabilă
+▪ 7: Sortare ascendentă categorie
+▪ 8: Sortare descendentă categorie
+o Fișier implicat: taskuri.txt
+o Funcționalitate: Sortare și afișare taskuri în funcție de criteriile selectate.
+"""
+
+import pandas as pd
+
+
+def sort_task(file, sort_type):
+    df = pd.read_csv(file)
+    if sort_type == 1:
+        sdf = df.sort_values(by=['name'])
+    elif sort_type == 2:
+        sdf = df.sort_values(by=['name'], ascending=False)
+    elif sort_type == 3:
+        sdf = df.sort_values(by=['max_date'])
+    elif sort_type == 4:
+        sdf = df.sort_values(by=['max_date'], ascending=False)
+    elif sort_type == 5:
+        sdf = df.sort_values(by=['assignee'])
+    elif sort_type == 6:
+        sdf = df.sort_values(by=['assignee'], ascending=False)
+    elif sort_type == 7:
+        sdf = df.sort_values(by=['category'])
+    elif sort_type == 8:
+        sdf = df.sort_values(by=['category'], ascending=False)
+    return sdf
+
+print(sort_task("taskuri.csv", 8))
